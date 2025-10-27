1f762dc05a65679bfd775fad68811c4f3523f6e3
PPeraltaUNAHUR <patricioagustin.peralta@estudiantes.unahur.edu.ar>
2025-10-27 19:38:29 -0300

    se agregan los metodos de SistemaLogistica

--- src/core/SistemaLogistica.h
+++ src/core/SistemaLogistica.h
@@ -1,50 +1,52 @@
 #pragma once
 #include <string>
 #include <vector>
-#include "Centro.h"
-#include "Envio.h"
-#include "Grafo.h"
-#include "TablaHash.h"
-#include "ABB.h"
-#include "Camino.h"
-#include "Fecha.h"
+#include "../models/Centro.h"
+#include "../models/Envio.h"
+#include "../graph/Grafo.h"
+#include "../structures/TablaHash.h"
+#include "../structures/ABB.h"
+#include "../models/Camino.h"
+#include "../utils/Fecha.h"
 
 using namespace std;
 
 class SistemaLogistica {
 private:
-    // Estructuras principales
+
+    //Estructuras principales
     TablaHash<string, Centro*> centros;
     Grafo red;
     
-    // Índices para búsquedas y análisis
+    //Índices para búsquedas y análisis
     TablaHash<string, ABB<Fecha, vector<Envio>>> enviosPorCentro;
     TablaHash<string, vector<Envio>> indicePaquetes;
     TablaHash<string, vector<Envio>> indiceClientes;
 
 public:
-    // ----- Constructores / Destructor -----
-    SistemaLogistica();
-    ~SistemaLogistica();
+    //Constructor.
 
-    // ----- Gestión de Centros -----
+    SistemaLogistica();
+    ~SistemaLogistica();    
+    
+    //Centros.
+
     void cargarCentros(const string& ruta);
     void cargarConexiones(const string& ruta);
     void cargarEnvios(const string& ruta);
-
     bool agregarCentro(const Centro& centro);
     bool eliminarCentro(const string& codigo);
     Centro* obtenerCentro(const string& codigo);
     vector<Centro*> listarCentros(const string& criterio);
 
-    // ----- Rutas y caminos -----
+    //Caminos.
+
     Camino caminoMinimo(const string& origen, const string& destino);
 
-    // ----- Análisis de Envíos -----
+    //Métodos de envíos.
+
     vector<Envio> enviosEnRango(const string& codigo, const Fecha& desde, const Fecha& hasta);
     vector<Centro*> detectarSobrecarga(int maximo);
     vector<Envio> buscarPorPaquete(int idPaquete);
 
-    // ----- Utilidades -----
-    void mostrarResumenGeneral() const;
 };
