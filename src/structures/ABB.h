1ae3e14c7a4e78d87d2c8e8ac0afe29433a13285
PPeraltaUNAHUR <patricioagustin.peralta@estudiantes.unahur.edu.ar>
2025-10-28 21:39:18 -0300

    se agregan metodos y atributos de algunas de las clases

--- src/structures/ABB.h
+++ src/structures/ABB.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <vector>
+using namespace std;
+
+class ABB {
+private:
+    //Acá faltan los atributos de la clase
+
+public:
+    ABB();
+    ~ABB();
+
+    //Acá faltan los métodos de la clase
+};
